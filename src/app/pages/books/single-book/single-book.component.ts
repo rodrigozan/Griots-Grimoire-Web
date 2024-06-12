cc8a6eda106fbfa1aa130af87acd4d5d68778635
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-12 16:25:46 -0300

    Created project structure

---
+++ src/app/pages/books/single-book/single-book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-book.component.html',
+  styleUrl: './single-book.component.scss'
+})
+export class SingleBookComponent {
+
+}
