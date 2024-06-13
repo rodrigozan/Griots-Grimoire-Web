8a39c348084ed7b0ea3f713065d12dd14278f4b7
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-13 16:34:14 -0300

    Created Header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { CommonModule } from '@angular/common'
 
+import { HeaderComponent } from './components/header/header.component'
 import { HomeComponent } from './pages/home/home.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HomeComponent], 
+  imports: [RouterOutlet, CommonModule, HomeComponent, HeaderComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/home/timeline/timeline.component.ts
+++ src/app/components/home/timeline/timeline.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-timeline',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss'
 })
 export class TimelineComponent {
+  dateNow = Date.now()
+  infos = [
+    {
+      img: "",
+      text: "Texto de teste",
+      author: "Autor 1",
+      data: this.dataConvert(this.dateNow)
+    }
+  ]
 
+  dataConvert(newData: number){
+    const data = new Date(newData);
+    const d = data.toLocaleDateString('pt-BR');
+    const t = data.toLocaleTimeString('pt-BR');
+    return `${d} - ${t}`
+  }
 }
