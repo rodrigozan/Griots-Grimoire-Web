dd85d39a218112aecca93c5f7b3c39b9ecb2ac35
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-13 14:56:15 -0300

    Add code logic to show components in Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,25 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 
+import { RegisterComponent } from '../../components/home/register/register.component'
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RegisterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  title = 'Griots Grimoire'      
+  title = 'Griots Grimoire' 
+  isLogged = false
+
+  ngOnInit() {
+    if(this.isLogged == true) this.goToRegister()
+  }
+  
+  public goToRegister() {
+    this.isLogged = true
+    console.log('ok, button started')
+  }
 }
