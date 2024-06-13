b94f541dc0f7f319c5ac6375407d1295cf64255f
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-13 11:46:41 -0300

    Add CommonModule and testing NgIf directive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   title = 'Griots Grimoire'
+  itsTrue = false
 }
