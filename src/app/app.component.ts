6de7a234134fcf5310c06dc9cecfc6e56d0dcd92
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-06-13 10:03:04 -0300

    Import Home Component in App Component ts and html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { BooksComponent } from './pages/books/books.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BooksComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'GriotsGrimoire';
+  title = 'Griots Grimoire';
 }
